bd9d2a962d0314b12936883c789daecd26139d0e
Евгения <bakhaeva@gmail.com>
2022-09-01 20:34:42 +0300

    Задача 27

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 class MyNewClass
 {
-       //Напишите цикл, который принимает на вход два числа (A и B)
+    //Задача 25. Напишите цикл, который принимает на вход два числа (A и B)
     // и возводит число A в натуральную степень B.
     public static  int MyPow(int numA, int numB)
     {
@@ -12,17 +12,32 @@ class MyNewClass
         return result;
     }
 
+    ///Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+    public static int SumNum( int num)
+    {
+        int result = 0;
+        while (num > 0)
+        {
+            result +=num % 10;
+            num /= 10;
+        } 
+        return result;
+    }
+
     static void Main()
     {
 
         Console.WriteLine("Введите первое число");
         int numberA= Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Введите второе число");
-        int numberB= Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Введите второе число");
+        //int numberB= Convert.ToInt32(Console.ReadLine());
 
-       int PowAB = MyPow(numberA, numberB);
-       Console.WriteLine(PowAB);
+      // int PowAB = MyPow(numberA, numberB);
+      // Console.WriteLine(PowAB);
+
+      int Summa = SumNum(numberA);
+      Console.WriteLine(Summa);
     }
 
  
