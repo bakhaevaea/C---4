cf9203961cc297cf1bf4511408bc7b962d55d251
Евгения <bakhaeva@gmail.com>
2022-09-02 23:43:57 +0300

    Задача 29

--- Program.cs
+++ Program.cs
@@ -24,11 +24,35 @@ class MyNewClass
         return result;
     }
 
+    //  Задача 29: Напишите программу, которая задаёт массив из введенного числа и выводит их на экран.
+    // пользователь вводит число элементов в массиве и минимальное, и максимальное значение .
+    //  надо заполнить массив рандоными данными от минимального до максимального значение .
+    //  И важное уточнениее , делать все в методе который возвращает массив
+    public static int[] NewArr()
+    {
+        System.Console.WriteLine("Введите размер массива ");
+        int Len = int.Parse( Console.ReadLine());
+        
+        System.Console.WriteLine("Введите минимальное значение ");
+        int MinVal = int.Parse( Console.ReadLine());
+        
+        System.Console.WriteLine("Введите максимальное значение ");
+        int MaxVal = int.Parse( Console.ReadLine());
+        
+        int [] MyArr = new int [Len];
+        for (int i = 0; i < Len; i++)
+        {
+            MyArr[i] = new Random().Next(MinVal, MaxVal+1);
+        }
+        return MyArr;
+    }
+
+
     static void Main()
     {
 
-        Console.WriteLine("Введите первое число");
-        int numberA= Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Введите первое число");
+        // int numberA= Convert.ToInt32(Console.ReadLine());
 
         // Console.WriteLine("Введите второе число");
         //int numberB= Convert.ToInt32(Console.ReadLine());
@@ -36,8 +60,12 @@ class MyNewClass
       // int PowAB = MyPow(numberA, numberB);
       // Console.WriteLine(PowAB);
 
-      int Summa = SumNum(numberA);
-      Console.WriteLine(Summa);
+        //   int Summa = SumNum(numberA);
+        //   Console.WriteLine(Summa);
+
+        int [] Array = NewArr();
+        System.Console.WriteLine(String.Join("  ", Array));
+
     }
 
  
